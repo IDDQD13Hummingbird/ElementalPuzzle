cccebe8ad8683136340054061ce7b306e7951140
Edvard Andreassen <mitori0211@gmail.com>
2024-10-21 22:30:03 +0200

    Added character
    Added a game character with a mesh component that will be controllable by clicking on a tile in the grid system

---
+++ Source/Yliaster/Private/MyCharacter.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Creating the Root component
+	CharRoot = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Root"));
+	RootComponent = CharRoot;
+
+	// Creating the Mesh component
+	CharMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	CharMesh->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Yliaster/Public/MyCharacter.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Components/BoxComponent.h"
+
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class YLIASTER_API AMyCharacter : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	// COMPONENTS
+	
+
+	//Character's Mesh
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	UStaticMeshComponent* CharMesh;
+
+	//Character's Root Component
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	UCapsuleComponent* CharRoot;
+
+	//
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Component")
+	UBoxComponent* Target;
+
+
+	// VARIABLES
+
+	// Base movement speed of the coontrolled character
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable")
+	float BaseMoveSpeed = 10.0f;
+
+};
