2bc39a5197b21e3c252ccd6227ed389b1f0f48da
TommyB-DontMindMe <142879081+TommyB-DontMindMe@users.noreply.github.com>
2024-11-03 14:12:02 +0100

    Implemented a simple wall

--- Source/Yliaster/Private/Core/VVGridActorComponent.cpp
+++ Source/Yliaster/Private/Core/VVGridActorComponent.cpp
@@ -4,30 +4,21 @@
 #include "Core/VVGridActorComponent.h"
 #include "Core/VVTile.h"
 
+UVVGridActorComponent::UVVGridActorComponent()
+{
+	CostModifier = 0;
+}
+
 void UVVGridActorComponent::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (UVVTile* OverlappedTile = Cast<UVVTile>(OtherComp))
-	{
-		if (CostModifier < 0)
-			OverlappedTile->TraversalCost *= -1;
-		else
-			OverlappedTile->TraversalCost += CostModifier;
-
-		OverlappedTile->TileClickDelegate.AddDynamic(this, &UVVGridActorComponent::TileClicked);
-	}
+		ModifyTile(OverlappedTile);
 }
 
 void UVVGridActorComponent::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	if (UVVTile* OverlappedTile = Cast<UVVTile>(OtherComp))
-	{
-		if (CostModifier < 0)
-			OverlappedTile->TraversalCost *= -1;
-		else
-			OverlappedTile->TraversalCost -= CostModifier;
-
-		OverlappedTile->TileClickDelegate.RemoveDynamic(this, &UVVGridActorComponent::TileClicked);
-	}
+		RevertTile(OverlappedTile);
 }
 
 void UVVGridActorComponent::TileClicked(UVVTile* TileClicked, int32 X, int32 Y, FKey ButtonPressed)
@@ -35,8 +26,45 @@ void UVVGridActorComponent::TileClicked(UVVTile* TileClicked, int32 X, int32 Y, 
 	ObjectClickedDelegate.Broadcast(ButtonPressed);
 }
 
+void UVVGridActorComponent::ModifyTile(UVVTile* TargetTile)
+{
+	if (CostModifier < 0)
+		TargetTile->TraversalCost *= -1;
+	else
+		TargetTile->TraversalCost += CostModifier;
+
+	TargetTile->TileClickDelegate.AddDynamic(this, &UVVGridActorComponent::TileClicked);
+}
+
+void UVVGridActorComponent::RevertTile(UVVTile* TargetTile)
+{
+	if (CostModifier < 0)
+		TargetTile->TraversalCost *= -1;
+	else
+		TargetTile->TraversalCost -= CostModifier;
+
+	TargetTile->TileClickDelegate.RemoveDynamic(this, &UVVGridActorComponent::TileClicked);
+}
+
+
+
 void UVVGridActorComponent::BeginPlay()
 {
+	Super::BeginPlay();
+
 	OnComponentBeginOverlap.AddDynamic(this, &UVVGridActorComponent::OnBeginOverlap);
 	OnComponentEndOverlap.AddDynamic(this, &UVVGridActorComponent::OnEndOverlap);
+
+	TArray<UPrimitiveComponent*> OverlappedComponents;
+	GetOverlappingComponents(OverlappedComponents);
+	for (UPrimitiveComponent* Component : OverlappedComponents)
+	{
+		if (UVVTile* TileComponent = Cast<UVVTile>(Component))
+			ModifyTile(TileComponent);
+	}
+}
+
+void UVVGridActorComponent::SetCost(int32 NewCost)
+{
+	CostModifier = NewCost;
 }
